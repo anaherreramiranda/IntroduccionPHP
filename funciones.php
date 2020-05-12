2e79c38a62e829d0777c4b9e4846e8098b81258f
anaherreramiranda <anac3714@hotmail.com>
2020-05-12 00:54:04 -0500

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -1,4 +1,21 @@
 <?php
 
+#funciones sin parametros
+function saludo(){
+    echo "Hola ANA <br>";
+}
+saludo();
+
+#funciones con parametros
+function despedida($adios){
+    echo $adios."<br>";
+}
+despedida("Adios ANA");
+
+#funciones con retorno
+function retorno($retornar){
+    return $retornar;
+}
+echo retorno("Esto es un retorno");
 
 ?>
